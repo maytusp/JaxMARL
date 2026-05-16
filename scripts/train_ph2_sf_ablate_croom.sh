b03acea8b97c41012412442b434e454eb646f7bf
maytusp <maytusp@gmail.com>
2026-05-16 23:58:58 +0800

    fixed typos in train_ph2_sf_ablate.sh

--- scripts/train_ph2_sf_ablate_croom.sh
+++ scripts/train_ph2_sf_ablate_croom.sh
@@ -27,7 +27,6 @@ for layout in "${layouts[@]}"; do
       +ENV_KWARGS.front_obs=true \
       ++PERSPECTIVE_TRANSFORM=false \
       ++PRETRAINED_CHECKPOINTS_PREFIX="checkpoints/single_sf_frontobs_sf${sf_coef}/" \
-      ++CHECKPOINTS_PREFIX="checkpoints/ph2sf_ablate}/"
+      ++CHECKPOINTS_PREFIX="checkpoints/ph2sf_ablate/"
   done
 done
-
